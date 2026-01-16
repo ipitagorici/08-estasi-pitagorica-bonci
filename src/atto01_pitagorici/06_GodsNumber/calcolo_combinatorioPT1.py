9d0b08dadb058e8c10ff686ca1266b1ee2097044
silentShulk <marcostrada258@gmail.com>
2026-01-16 15:42:35 +0100

    Addedd new animations to calcolo combinatorio, there are problems but I need to go

--- src/atto01_pitagorici/06_GodsNumber/calcolo_combinatorioPT1.py
+++ src/atto01_pitagorici/06_GodsNumber/calcolo_combinatorioPT1.py
@@ -1,57 +1,99 @@
 from manim import *
 
+
 class CalcoloCombinatorio(Scene):
     def construct(self):
-        cube = ImageMobject("assets/imgs/rubiks-cube.png")
-        self.play(FadeIn(cube))
-        self.play(cube.animate.scale(.3).to_edge(UP))
+        # --------------------- #
+        #   DEFINING MOBJECTS   #
+        # --------------------- #
 
+        cube = ImageMobject("assets/imgs/rubiks-cube.png")
+
+        # DEFINING PIECES OF THE CUBE
         s_text = Tex(r"$12 \times$ \textsc{Spigoli}")
         a_text = Tex(r"$8 \times$ \textsc{Angoli}")
         c_text = Tex(r"$6 \times$ \textsc{Centri}")
-
+        
         s_img = ImageMobject("assets/imgs/spigoli.png").scale(0.1)
         a_img = ImageMobject("assets/imgs/angoli.png").scale_to_fit_height(s_img.height)
         c_img = ImageMobject("assets/imgs/centri.png").scale_to_fit_height(s_img.height)
-        
+
         spigoli = Group(s_text, s_img)
         angoli = Group(a_text, a_img)
         centri = Group(c_text, c_img)
+        
+        # ARRANGING PIECES
+        for g in [spigoli, angoli, centri]:
+            g.arrange_in_grid(cols=1, buff=0.2)
 
-        for g in [ spigoli, angoli, centri ]:
-            g.arrange_in_grid(cols=1, buff=.2)
-
-        row = Group(centri, angoli, spigoli)\
-            .arrange_in_grid(rows=1)\
-            .next_to(cube, DOWN, buff=.5)
-        
-        for g in row:
-            self.play(FadeIn(g))
-            
-            
-        self.wait()
-        
-        
-        self.next_section()
-        
-        
-        self.play(FadeOut(cube))
-        
-        self.play(row.animate.scale(0.6))
-        text_fade_out = AnimationGroup(
-            FadeOut(row[0][0]),
-            FadeOut(row[1][0]),
-            FadeOut(row[2][0])
+        pieces = (
+            Group(centri, angoli, spigoli)
+            .arrange_in_grid(rows=1)
+            .next_to(cube, DOWN, buff=0.5)
         )
         
-        self.play(row.animate.arrange_in_grid(cols=1, buff=0).center().to_edge(LEFT))        
-        self.play(text_fade_out)
+        # NUMBERS FOR CALCULATIONS
+        calcolo_centri = Tex(r"Posizione //// Costante").next_to(centri, RIGHT)
+        calcolo_angoli = Tex(r"8! combinazioni //// $3^{8}$ colori").next_to(angoli, RIGHT)
+        calcolo_spigoli = Tex(r"12! combinazioni //// $2^{12}$ colori").next_to(spigoli, RIGHT)
+        
+        totale_mosse = Tex(r"519.024.039.293.878.272.000 // (519 quintilioni)", font_size=70)
+        
+        
+        
+        # -------------- #
+        #   ANIMATIONS   #
+        # -------------- #
+
+        self.play(FadeIn(cube))
+        self.play(cube.animate.scale(0.3).to_edge(UP))
+        
+        for piece in pieces:
+            self.play(FadeIn(piece))
+            print(piece.get_x(), piece.get_y())
+
+        self.wait()
+
+
+        self.next_section() # NEXT SECTION --------------------------------------------------------------
+
+
+        self.play(FadeOut(cube))
+
+        self.play(pieces.animate.scale(0.6))
+        self.play(pieces.animate.arrange_in_grid(cols=1, buff=0).center().to_edge(LEFT))
+        
+        calcolo_centri.next_to(centri, RIGHT)
+        calcolo_angoli.next_to(angoli, RIGHT)
+        calcolo_spigoli.next_to(spigoli, RIGHT)
+         
+
+        self.wait()
+
+
+        self.next_section() # NEXT SECTION -------------------------------------------------------------
 
         
+        self.play(FadeIn(calcolo_centri))
+
+        
+        self.next_section() # NEXT SECTION --------------------------------------------------------------
+        
+        
+        self.play(FadeIn(calcolo_angoli))
+         
+         
+        self.next_section() # NEXT SECTION -------------------------------------------------------------
+         
+         
+        self.play(FadeIn(calcolo_spigoli))
+        
+        
+        self.next_section() # NEXT SECTION --------------------------------------------------------------
+        
+        
+        self.play(FadeOut(calcolo_centri), FadeOut(calcolo_angoli), FadeOut(calcolo_spigoli))
+        self.play(FadeOut(pieces))
+        self.play(FadeIn(totale_mosse))
+        
         self.wait(2)
-        
-        
-        self.next_section()
-        
-        
-        
