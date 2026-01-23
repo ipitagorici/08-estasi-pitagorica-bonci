b8c841f554984bed36b8615e7ab6ede1e1af7369
silentShulk <marcostrada258@gmail.com>
2026-01-23 20:42:54 +0100

    STarted working on storie pitagoriche

---
+++ src/atto03_finale/storie_pitagoriche.py
@@ -0,0 +1,16 @@
+from manim import *
+
+class StoriePitagoriche:
+    def construct(self):
+        immagini_pitagoriche = Group(
+            ImageMobject("assets/imgs/storie_pitagoriche/2024_07_29 DSCF1004.jpg"),
+            ImageMobject("assets/imgs/storie_pitagoriche/2024_07_30 DSCF1036.jpg"),
+            ImageMobject("assets/imgs/storie_pitagoriche/2025_10_30 225841 DSCF5687.jpg"),
+            ImageMobject("assets/imgs/storie_pitagoriche/2025_10_30 230840 DSCF5709.jpg"),
+            ImageMobject("assets/imgs/storie_pitagoriche/2025_11_14 DSCF5855.jpg4"),
+            ImageMobject("assets/imgs/storie_pitagoriche/_DSC1729.JPG"),
+            ImageMobject("assets/imgs/storie_pitagoriche/_DSC9430.JPG"),
+            ImageMobject("assets/imgs/storie_pitagoriche/DSC_0067.JPG"),
+            ImageMobject("assets/imgs/storie_pitagoriche/IMG-20240730-WA0017.jpg"),
+            ImageMobject("assets/imgs/storie_pitagoriche/IMG-20250125-WA0000.jpg"),
+        )
