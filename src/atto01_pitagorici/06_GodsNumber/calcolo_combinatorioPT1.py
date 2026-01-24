e3f7d6d1f0c334915a758ffc69fe71ca5eb46266
Nickolausen <nicholas.magi24@gmail.com>
2026-01-24 18:32:32 +0100

    habemus brasinim

--- src/atto01_pitagorici/06_GodsNumber/calcolo_combinatorioPT1.py
+++ src/atto01_pitagorici/06_GodsNumber/calcolo_combinatorioPT1.py
@@ -1,5 +1,5 @@
 from manim import *
-
+from src.custom_mobjects import BoheldText
 
 class CalcoloCombinatorio(Scene):
     def construct(self):
@@ -7,39 +7,55 @@ class CalcoloCombinatorio(Scene):
         #   DEFINING MOBJECTS   #
         # --------------------- #
 
-        cube = ImageMobject("assets/imgs/rubiks-cube.png")
+        cube = ImageMobject("src/assets/imgs/rubiks-cube.png")
 
         # DEFINING PIECES OF THE CUBE
-        s_text = Tex(r"$12 \times$ \textsc{Spigoli}")
-        a_text = Tex(r"$8 \times$ \textsc{Angoli}")
-        c_text = Tex(r"$6 \times$ \textsc{Centri}")
+        s_text = MathTex(r"12 \times \textsc{Spigoli}", substrings_to_isolate=["12"])\
+            .set_color_by_tex("12", YELLOW)
+        a_text = MathTex(r"8 \times \textsc{Angoli}", substrings_to_isolate=["8"])\
+            .set_color_by_tex("8", TEAL)
+        c_text = MathTex(r"6 \times \textsc{Centri}", substrings_to_isolate=["6"])\
+            .set_color_by_tex("6", GREEN)
         
-        s_img = ImageMobject("assets/imgs/spigoli.png").scale(0.1)
-        a_img = ImageMobject("assets/imgs/angoli.png").scale_to_fit_height(s_img.height)
-        c_img = ImageMobject("assets/imgs/centri.png").scale_to_fit_height(s_img.height)
+        s_img = ImageMobject("src/assets/imgs/spigoli.png").scale(0.4)
+        a_img = ImageMobject("src/assets/imgs/angoli.png").scale_to_fit_height(s_img.height)
+        c_img = ImageMobject("src/assets/imgs/centri.png").scale_to_fit_height(s_img.height)
 
         spigoli = Group(s_text, s_img)
         angoli = Group(a_text, a_img)
         centri = Group(c_text, c_img)
         
         # ARRANGING PIECES
-        for g in [spigoli, angoli, centri]:
-            g.arrange_in_grid(cols=1, buff=0.2)
+        for g in [ spigoli, angoli, centri ]:
+            g.arrange_in_grid(cols=1, cell_alignment=ORIGIN, buff=0.25)
 
         pieces = (
             Group(centri, angoli, spigoli)
-            .arrange_in_grid(rows=1)
-            .next_to(cube, DOWN, buff=0.5)
+                .arrange_in_grid(rows=1)
+                .align_to(cube, DOWN)
         )
         
         # NUMBERS FOR CALCULATIONS
-        calcolo_centri = Tex(r"Posizione //// Costante").next_to(centri, RIGHT)
-        calcolo_angoli = Tex(r"8! combinazioni //// $3^{8}$ colori").next_to(angoli, RIGHT)
-        calcolo_spigoli = Tex(r"12! combinazioni //// $2^{12}$ colori").next_to(spigoli, RIGHT)
-        
-        totale_mosse = Tex(r"519.024.039.293.878.272.000 // (519 quintilioni)", font_size=70)
-        
-        
+        calcolo_centri = Tex(r"Posizione costante")
+        calcolo_angoli = VGroup(
+            MathTex(r"8!\ \text{ posizioni }", substrings_to_isolate=[r"8"])\
+                .set_color_by_tex(r"8", TEAL),
+            Tex(r"$ \times $"),
+            MathTex(r"3^8 \text{ colori}", substrings_to_isolate=[r"8"])\
+                .set_color_by_tex(r"8", TEAL)
+        ).arrange_in_grid(rows=1)
+        calcolo_spigoli = VGroup(
+            MathTex(r"12! \text{ posizioni}", substrings_to_isolate=[r"12"])\
+                .set_color_by_tex("12", YELLOW),
+            Tex(r"$ \times $"),
+            MathTex(r"2^{12} \text{ colori}", substrings_to_isolate=[r"12"])\
+                .set_color_by_tex("12", YELLOW)
+        ).arrange_in_grid(rows=1)
+
+        totale_mosse = Group(
+            BoheldText(r"519.024.039.293.878.272.000").scale_to_fit_width(self.camera.frame_width - 3),
+            Tex(r"(519 \textsc{quintilioni})", font_size=50)
+        ).arrange_in_grid(cols=1).center()
         
         # -------------- #
         #   ANIMATIONS   #
@@ -54,46 +70,39 @@ class CalcoloCombinatorio(Scene):
 
         self.wait()
 
-
         self.next_section() # NEXT SECTION --------------------------------------------------------------
-
 
         self.play(FadeOut(cube))
 
         self.play(pieces.animate.scale(0.6))
-        self.play(pieces.animate.arrange_in_grid(cols=1, buff=0).center().to_edge(LEFT))
+        self.play(pieces.animate.arrange_in_grid(cols=1, buff=0.1)\
+                .center()\
+                .to_edge(LEFT))
+
+        calcoli = Group(
+            centri, calcolo_centri, 
+            angoli, calcolo_angoli, 
+            spigoli, calcolo_spigoli)\
+            .arrange_in_grid(rows=3, cell_alignment=LEFT, buff=(LARGE_BUFF, MED_SMALL_BUFF))
         
-        calcolo_centri.next_to(centri, RIGHT)
-        calcolo_angoli.next_to(angoli, RIGHT)
-        calcolo_spigoli.next_to(spigoli, RIGHT)
-         
+        # ARRANGING PIECES — again
+        for g in [ spigoli, angoli, centri ]:
+            g.arrange_in_grid(cols=1, cell_alignment=ORIGIN, buff=0.25)
 
         self.wait()
-
-
         self.next_section() # NEXT SECTION -------------------------------------------------------------
 
-        
-        self.play(FadeIn(calcolo_centri))
-
-        
-        self.next_section() # NEXT SECTION --------------------------------------------------------------
-        
-        
-        self.play(FadeIn(calcolo_angoli))
-         
-         
-        self.next_section() # NEXT SECTION -------------------------------------------------------------
-         
-         
-        self.play(FadeIn(calcolo_spigoli))
-        
-        
-        self.next_section() # NEXT SECTION --------------------------------------------------------------
-        
+        # -------------- #
+        #     NUMERI     #
+        # -------------- #
+        for text_idx in range(1, len(calcoli), 2):
+            self.play(FadeIn(calcoli[text_idx]))
+            self.next_section() # NEXT SECTION --------------------------------------------------------------
         
         self.play(FadeOut(calcolo_centri), FadeOut(calcolo_angoli), FadeOut(calcolo_spigoli))
         self.play(FadeOut(pieces))
-        self.play(FadeIn(totale_mosse))
+        self.wait()
+        self.play(Write(totale_mosse[0]))
+        self.play(FadeIn(totale_mosse[1]))
         
         self.wait(2)
